0696fe33ad0cb43b28d8cec1c21d591cb32e9baa
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-11 10:24:51 -0300

    Aprimora métodos para implemnetação abstrata

--- src/main/java/org/vedu/managerhubfinance/persistence/repository/CrudRepository.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/repository/CrudRepository.java
@@ -1,72 +1,114 @@
 package org.vedu.managerhubfinance.persistence.repository;
 
 import java.util.List;
+import java.util.Optional;
+
+import jakarta.persistence.LockModeType;
 
 /**
  * This interface defines the contract for a CRUD repository.
- * It is a generic interface that can be used with any type of primary key (K) and entity class (V).
+ * It is a generic interface that can be used with any type of primary key (ID) and entity class (E).
  *
- * @param <K> the type of the primary key
- * @param <V> the type of the entity class
+ * @param <ID> the type of the primary key
+ * @param <E> the type of the entity class
  */
-public interface CrudRepository<K, V> {
+public interface CrudRepository<ID, E> {
 	/**
 	  * This method is used to find a single entity in the repository.
 	  *
 	  * @param key the key of the entity to find
-	  * @param entity the class of the entity to find
 	  * @return the found entity
 	  */
-	public V findSingleEntity(K key, Class<V> entity);
+	public Optional<E> findSingleEntity(ID key);
+	
+	/**
+	 * This method is used to find a single entity in the repository with a lock
+	 * mode.
+	 *
+	 * @param key      the key of the entity to find
+	 * @param lockMode the lock mode to use
+	 * @return the found entity
+	 */
+	public Optional<E> findSingleEntity(ID key, LockModeType lockMode);
 	
 	/**
 	  * This method is used to find a list of entities in the repository.
 	  *
-	  * @param entity the class of the entities to find
 	  * @return the list of found entities
 	  */
-	public List<V> findListEntity(Class<V> entity);
+	public List<E> findListEntity();
+	
+	/**
+	 * This method is used to find a list of entities in the repository with a lock
+	 * mode.
+	 *
+	 * @param lockMode the lock mode to use
+	 * @return the list of found entities
+	 */
+	public List<E> findListEntity(LockModeType lockMode);
+	
+	/**
+	 * This method is used to find a single reference of an entity in the repository.
+	 *
+	 * @param key the key of the entity to find
+	 * @return the found entity reference
+	 */
+	public E findSingleReference(ID key);
+	
+	/**
+	 * This method is used to get the count of entities in the repository.
+	 *
+	 * @return the count of entities
+	 */
+	public Long getCount();
 	
 	/**
 	  * This method is used to save an entity in the repository.
 	  *
 	  * @param entity the entity to save
 	  */
-	public void save(V entity);
+	public void save(E entity);
 	
 	 /**
 	  * This method is used to save a list of entities in the repository.
 	  *
 	  * @param entities the list of entities to save
 	  */
-	public void saveAll(List<V> entities);
+	public void saveAll(List<E> entities);
 	
 	/**
 	 * This method is used to update an entity in the repository.
 	 *
 	 * @param entity the entity to update
 	 */
-	public void update(V entity);
+	public void makePersistent(E entity);
 	
 	/**
 	 * This method is used to update a list of entities in the repository.
 	 *
 	 * @param entities the list of entities to update
 	 */
-	public void updateAll(List<V> entities);
+	public void makePersistentAll(List<E> entities);
 	
 	/**
 	 * This method is used to delete an entity from the repository.
 	 *
 	 * @param entity the entity to delete
 	 */
-	public void delete(V entity);
+	public void makeTransient(E entity);
 	
 	/**
 	 * This method is used to delete a list of entities from the repository.
 	 *
 	 * @param entities the list of entities to delete
 	 */
-	public void deleteAll(List<V> entities);
+	public void makeTransientAll(List<E> entities);
 	
+	/**
+	 * This method is used to check the version of an entity.
+	 *
+	 * @param entity      the entity to check
+	 * @param forceUpdate whether to force an update
+	 */
+	public void checkVersion(E entity, boolean forceUpdate);
 }
