26dde510821a10bb144c43a453ec47a4fe0a9ddb
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-11 11:04:39 -0300

    Modifica estratégia de transações em ambiente Java SE

--- src/main/java/org/vedu/managerhubfinance/service/transaction/RepositoryTransaction.java
+++ src/main/java/org/vedu/managerhubfinance/service/transaction/RepositoryTransaction.java
@@ -1,80 +1,40 @@
-package org.vedu.managerhubfinance.service.impl;
+package org.vedu.managerhubfinance.service.transaction;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.PersistenceUnit;
+import lombok.Data;
 
-
-public class RepositoryTransaction<K, V> {
+@Data
+public class RepositoryTransaction<E> {
 	
-	@PersistenceUnit(unitName = "manager_hub")
-	private EntityManagerFactory emf;
+	private EntityManager entityManager;
 	
-	public Optional<List<V>> executeFindInTransaction(Function<EntityManager, List<V>> action) {
-		EntityTransaction et = null;
-		try (EntityManager em = emf.createEntityManager()) {
-			et = em.getTransaction();
-			et.begin();
-			
-			List<V> result = action.apply(em);
-			
-			result.forEach(em::detach);
-			et.commit();
-			return Optional.ofNullable(result);
-		} catch (Exception e) {
-			System.err.println("Erro ao tentar obter entidade: " + e.getMessage());
-			throw e;
-        }
-    }
-	
-	public void executeSaveInTransaction(Consumer<EntityManager> action) {
-        EntityTransaction et = null;
-        try (EntityManager em = emf.createEntityManager()) {
-            et = em.getTransaction();
-            et.begin();
+	public void runInTransaction(Consumer<EntityManager> action) {
+        try (EntityManager em = entityManager) {
+            em.getTransaction().begin();
 
             action.accept(em);
 
-            et.commit();
+            em.getTransaction().commit();
         } catch (Exception e) {
-			System.err.println("Erro ao persistir entidade: " + e.getMessage());
+			System.err.println("Erro ao executar transação: " + e.getMessage());
 			throw e;
         }
     }
 	
-	public void executeUpdateInTransaction(Consumer<EntityManager> action) {
-		EntityTransaction et = null;
-		try (EntityManager em = emf.createEntityManager()) {
-			et = em.getTransaction();
-			et.begin();
-			
-			action.accept(em);
-			
-			et.commit();
-		} catch (Exception e) {
-			System.err.println("Erro ao atualizar entidade: " + e.getMessage());
-			throw e;
-		}
-	}
-	
-	public void executeDeleteInTransaction(Consumer<EntityManager> action) {
-		EntityTransaction et = null;
-		try (EntityManager em = emf.createEntityManager()) {
-			et = em.getTransaction();
-			et.begin();
-			
-			action.accept(em);
-			
-			et.commit();
-		} catch (Exception e) {
-			System.err.println("Erro ao deletar entidade: " + e.getMessage());
-			throw e;
-		}
+	public E callInTransaction(Function<EntityManager, E> action) {
+        try (EntityManager em = entityManager) {
+            em.getTransaction().begin();
+
+            E result = action.apply(em);
+
+            em.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            System.err.println("Erro ao executar transação: " + e.getMessage());
+            return null;
+        }
 	}
 }
