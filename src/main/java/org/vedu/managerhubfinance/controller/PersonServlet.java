86defb4a5c0a860cd99715cce506887c8a5ceb49
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-11 11:05:21 -0300

    Faz teste para a camada de lógica de negócios

--- src/main/java/org/vedu/managerhubfinance/controller/PersonServlet.java
+++ src/main/java/org/vedu/managerhubfinance/controller/PersonServlet.java
@@ -2,22 +2,17 @@ package org.vedu.managerhubfinance.controller;
 
 import java.io.IOException;
 
-import org.vedu.managerhubfinance.persistence.model.persongroup.Person;
-import org.vedu.managerhubfinance.persistence.repository.PersonDao;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/personServlet")
+@WebServlet("/employees-table")
 public class PersonServlet extends HttpServlet {
 	private static final long serialVersionUID = 7718788711864047538L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.getRequestDispatcher("/WEB-INF/view/employees.html").forward(request, response);
 	}
-	
-
 }
