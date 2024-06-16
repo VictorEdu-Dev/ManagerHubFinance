d487b3f5a4b3fc8f4c364bd20a29816399a98df8
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-16 02:31:02 -0300

    configure container initialization

--- src/main/java/org/vedu/managerhubfinance/controller/config/ContainerInitializer.java
+++ src/main/java/org/vedu/managerhubfinance/controller/config/ContainerInitializer.java
@@ -6,19 +6,20 @@ import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
+import jakarta.servlet.http.HttpServlet;
 
 public class ContainerInitializer implements ServletContainerInitializer {
 
-	@Override
-	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-	    if (c != null) {
-	        for (Class<?> clazz : c) {
-	            String servletName = clazz.getName();
-	            ServletRegistration.Dynamic registration = ctx.addServlet(servletName, clazz.asSubclass(jakarta.servlet.http.HttpServlet.class));
-	            registration.addMapping("/" + servletName);
-	        }
-	    }
-	}
-
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        if (c != null) {
+            for (Class<?> clazz : c) {
+                String servletName = clazz.getName();
+                ServletRegistration.Dynamic registration = ctx
+                		.addServlet(servletName,
+                		clazz.asSubclass(HttpServlet.class));
+                registration.addMapping("/" + servletName);
+            }
+        }
+    }
 }
-
