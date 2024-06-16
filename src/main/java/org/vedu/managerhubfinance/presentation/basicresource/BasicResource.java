f3cfdeeb4ad8fb7406bf8df2e1e3bb3edbfa8237
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-16 02:23:47 -0300

    fixed #2 - implements REST API to access basic resources

--- src/main/java/org/vedu/managerhubfinance/presentation/basicresource/BasicResource.java
+++ src/main/java/org/vedu/managerhubfinance/presentation/basicresource/BasicResource.java
@@ -1,19 +1,14 @@
-package org.vedu.managerhubfinance.service.basic;
+package org.vedu.managerhubfinance.presentation.basicresource;
 
+import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
-import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
-import org.vedu.managerhubfinance.persistence.model.groupbasic.Cep;
-import org.vedu.managerhubfinance.persistence.repository.base.impl.CepDAOImpl;
+import org.vedu.managerhubfinance.service.basic.BasicService;
 
-import jakarta.inject.Inject;
-import jakarta.persistence.PersistenceException;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
+import jakarta.ejb.EJB;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -21,67 +16,40 @@ import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/ceps")
-public class BasicResource {
-    private final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
-    @Inject
-    private CepDAOImpl cepDAO;
+@Path("/basic")
+public class BasicResource implements Serializable {
+	private static final long serialVersionUID = -8123149342539582297L;
+	private final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
+	@EJB
+    BasicService basicService;
+    
     @GET
-    @Path("/get-employee/{id}")
+    @Path("/cep/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Cep findCep(@PathParam("id") String id) {
-        logger.info("Getting coffee by id " + id);
-        return cepDAO.findSingleEntity(id)
+    public String findCityNameByCep(@PathParam("id") String cep) {
+        logger.info("Getting city by cep " + cep);
+        return basicService.getCityName(cep)
             .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
     }
-
+    
     @GET
-    @Produces("application/json")
-    public List<Cep> findAll() {
-        logger.info("Getting all coffee");
-        return cepDAO.findListEntity();
+    @Path("/state/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Integer findStateCodeByName(@PathParam("id") String name) {
+        logger.info("Getting state code by name " + name);
+        return basicService.getStateCode(name)
+            .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
     }
-
-    @POST
-    @Consumes("application/json")
-    @Produces("application/json")
-    public Cep create(Cep cep) {
-        logger.info("Creating coffee " + cep.getCep());
-        try{
-        	cepDAO.save(cep);
-            return cep;
-        }catch (PersistenceException ex){
-            logger.info("Error creating coffee " + cep.getCep());
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
-    }
-
-    @DELETE
-    @Path("{id}")
-    public void delete(@PathParam("id") Long id) {
-        logger.info("Deleting coffee by id " + id);
-        try{
-            cepDAO.makeTransientById(String.valueOf(id));
-        }catch (IllegalArgumentException e){
-            logger.info("Error deleting coffee by id " + id);
-            throw new WebApplicationException(Response.Status.NOT_FOUND);
-        }
-    }
-
-
-    @PUT
-    @Consumes("application/json")
-    @Produces("application/json")
-    public Cep update(Cep cep) {
-        logger.info("Updating coffee " + cep.getCep());
-        try{
-        	cepDAO.makePersistent(cep);
-            return cep;
-        }catch (PersistenceException ex){
-            logger.info("Error updating coffee " + cep.getCep());
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
+    
+    
+    @GET
+    @Path("/address/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, String> findAddressInfo(@PathParam("id") String cep) {
+        logger.info("Getting address info by cep " + cep);
+        return basicService.getAddressInfo(cep)
+            .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
     }
 }
