d8fbecff896745ef913681366d2d26248d0a8e6a
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-16 02:33:24 -0300

    empty test class

--- src/main/java/org/vedu/managerhubfinance/controller/api/Application.java
+++ src/main/java/org/vedu/managerhubfinance/controller/api/Application.java
@@ -2,53 +2,15 @@ package org.vedu.managerhubfinance.controller.api;
 
 import java.io.IOException;
 
-import org.vedu.managerhubfinance.persistence.model.groupbasic.Cep;
-import org.vedu.managerhubfinance.persistence.model.groupperson.Person;
-import org.vedu.managerhubfinance.persistence.model.groupperson.PersonAddress;
-import org.vedu.managerhubfinance.persistence.repository.base.impl.CepDAOImpl;
-import org.vedu.managerhubfinance.service.transaction.RepositoryTransaction;
-
 import com.opencsv.exceptions.CsvException;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Application {
-	private static Cep cep;
-	
 	public static void main(String[] args) throws IOException, CsvException {
 		try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("manager_hub")) {
 			
-			RepositoryTransaction<Cep> rt = new RepositoryTransaction<>();
-			rt.setEntityManager(emf.createEntityManager());
-			
-			CepDAOImpl cepDao = CepDAOImpl.getInstance();
-			
-			cep = rt.callInTransaction(em -> {
-				return cepDao.findSingleEntity("62010560").get();
-			});
-			
-			PersonAddress pa = PersonAddress
-					.builder()
-					.numberCep(cep)
-					.city(cep.getCity())
-					.state(cep.getCity().getState())
-					.billing("N")
-					.delivery("Y")
-					.correspondence("Y")
-					.complement("Apto 201")
-					.number("199")
-					.neighborhood("Centro")
-					.publicPlace("")
-					.build();
-			
-			Person p = Person
-					.builder()
-					.name("Victor Eduardo Pita Campos")
-					.site("VictorEdu-Dev.github.io")
-					.personAddress(pa)
-					.build();
-
 		} catch (Exception e) {
 		    System.err.println("Erro: " + e.getMessage());
 		}
