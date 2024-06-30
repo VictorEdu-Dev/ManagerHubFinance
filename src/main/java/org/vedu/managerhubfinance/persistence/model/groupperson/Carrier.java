2fc8a3324d6f05213a6a5bf2575f4e7f13840ea0
Victor Eduardo <victoreduardodev@gmail.com>
2024-06-30 15:06:20 -0300

    change data type mapping to @Temporal

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupperson/Carrier.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupperson/Carrier.java
@@ -13,6 +13,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -37,7 +39,7 @@ public class Carrier extends PropertiesEntity {
 	@JoinColumn(name = "person_id", nullable = false)
 	private Person person;
 	
-	@Basic
+	@Temporal(TemporalType.DATE)
 	@Column(name = "register_date", nullable = false)
 	private LocalDateTime registerDate;
 	
