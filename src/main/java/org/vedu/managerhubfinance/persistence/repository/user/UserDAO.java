0766723ea61ec2aa1cfcd689467051ecb1f07b35
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:37:40 -0300

    remove unnecessary entities after refactoring

--- src/main/java/org/vedu/managerhubfinance/persistence/repository/user/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package org.vedu.managerhubfinance.persistence.repository.user;
-
-import org.vedu.managerhubfinance.persistence.repository.CrudRepository;
-
-public interface UserDAO<ID, E> extends CrudRepository<ID, E> {
-	public E findByUsername(ID username);
-}
