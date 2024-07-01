6a29f0d105d48040134139a8f8ea838a08d20d32
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:03:41 -0300

    simplify the collaborator data model - add field for photo - remove desnecessary date fields - rename incorrectly namde fields

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Collaborator.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Collaborator.java
@@ -7,16 +7,19 @@ import org.vedu.managerhubfinance.persistence.model.groupperson.Person;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -27,13 +30,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = { "person", "jobTitle", "departament"})
-@Builder
 @Table(name = "collaborator")
 @Entity
-public class Collaborator extends PropertiesEntity {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "COLLABORATOR_TYPE", discriminatorType = DiscriminatorType.STRING)
+public abstract class Collaborator extends PropertiesEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Lob
+	@Column(name = "photo")
+	private byte[] photo;
 	
 	@OneToOne
 	@JoinColumn(name = "person_id", nullable = false)
@@ -47,40 +55,14 @@ public class Collaborator extends PropertiesEntity {
 	@JoinColumn(name = "departament_id", nullable = false)
 	private Departament departament;
 	
-	@Basic
-	@Column(name = "enrollment", nullable = false, unique = true, length = 100)
-	private String enrollment;
-	
-	@Basic
-	@Column(name = "registration_date", nullable = false)
-	private LocalDateTime registrationDate;
-	
-	@Basic
 	@Column(name = "admission_date", nullable = false)
 	private LocalDateTime admissionDate;
 	
 	@Basic
-	@Column(name = "resignation_date")
-	private LocalDateTime resignationDate;
+	@Column(name = "pis_pasep", length = 11)
+	private String pisPasep;
 	
 	@Basic
-	@Column(name = "ctps_number", length = 100)
-	private String ctpsNumber;
-	
-	@Basic
-	@Column(name = "ctps_serie", length = 100)
-	private String ctpsSerie;
-	
-	@Basic
-	@Column(name = "ctps_state", length = 100)
-	private String ctpsState;
-	
-	@Basic
-	@Column(name = "ctps_issue_date")
-	private LocalDateTime ctpsIssueDate;
-	
-	@Lob
-	@Column(name = "ctps_issue_agency", length = 100)
-	private String observation;
-
+	@Column(name = "ctps", length = 15)
+	private String ctps;
 }
