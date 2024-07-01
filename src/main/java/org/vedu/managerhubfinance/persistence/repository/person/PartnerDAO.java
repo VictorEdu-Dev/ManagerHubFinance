0356fe2ae5cfdd58350e565694130cf6efed5300
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:45:01 -0300

    create data access class for Partner

---
+++ src/main/java/org/vedu/managerhubfinance/persistence/repository/person/PartnerDAO.java
@@ -0,0 +1,9 @@
+package org.vedu.managerhubfinance.persistence.repository.person;
+
+import org.vedu.managerhubfinance.persistence.model.groupperson.Partner;
+
+import jakarta.data.repository.CrudRepository;
+
+public interface PartnerDAO extends CrudRepository<Partner, Long> {
+
+}
