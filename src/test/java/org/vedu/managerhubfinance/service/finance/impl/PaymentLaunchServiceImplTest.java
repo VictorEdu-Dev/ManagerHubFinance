4e642a7ab8a08d3ca93693bb7c49d7a355f32f53
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:45:34 -0300

    create test for PaymentLaunchServiceImpl

---
+++ src/test/java/org/vedu/managerhubfinance/service/finance/impl/PaymentLaunchServiceImplTest.java
@@ -0,0 +1,44 @@
+package org.vedu.managerhubfinance.service.finance.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PaymentLaunchServiceImplTest {
+
+	@Test
+	void testPaymentLaunchServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegisterAccountPayable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateAccountPayable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAccountPayable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListAccountPayable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListAccountPayableMapOfStringObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindBySourceDocumentsId() {
+		fail("Not yet implemented");
+	}
+
+}
