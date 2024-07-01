01535e71627e0bb1658d7a1baf5a96c44c872561
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:14:36 -0300

    remove field related to the deleted entity

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupperson/EducationLevel.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupperson/EducationLevel.java
@@ -1,30 +1,24 @@
 package org.vedu.managerhubfinance.persistence.model.groupperson;
 
-import java.util.List;
-
 import org.vedu.managerhubfinance.persistence.model.PropertiesEntity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(exclude = "personNatural")
 @Builder
 @Table(name = "education_level")
 @Entity
@@ -32,9 +26,6 @@ public class EducationLevel extends PropertiesEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@OneToMany(mappedBy = "educationLevel", fetch = FetchType.LAZY)
-	private List<PersonNatural> personNatural;
 	
 	@Basic
 	@Column(name = "name", nullable = false, length = 100)
