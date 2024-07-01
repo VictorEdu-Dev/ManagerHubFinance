4be2f66788770c8bcafd13b3c6601655ea484aa8
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:58:04 -0300

    define a seller model - extends collaborator class - add feilds for comission types and comissioning - add map fpr optional comission values - add field for discount limit - add field for numberOfInstallmentsInContract - add relationship field with users

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Seller.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Seller.java
@@ -1,18 +1,19 @@
 package org.vedu.managerhubfinance.persistence.model.groupcollaborator;
 
 import java.math.BigDecimal;
-
-import org.vedu.managerhubfinance.persistence.model.PropertiesEntity;
+import java.util.Map;
 
 import jakarta.persistence.Basic;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,24 +25,43 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(exclude = {"collaborator"})
+@ToString(exclude = {"collaborator", "users"})
 @Builder
-@Table(name = "seller")
 @Entity
-public class Seller extends PropertiesEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
+@DiscriminatorValue("SELLER")
+public class Seller extends Collaborator {
 	@OneToOne
 	@JoinColumn(name = "collaborator_id", nullable = false)
 	private Collaborator collaborator;
 	
-	@Basic
-	@Column(name = "comission", nullable = true)
-	private BigDecimal comission;
+	@Column(name = "commissioning")
+	@Enumerated(EnumType.STRING)
+	private Commissioning commissioning;
+	
+	@Column(name = "commission_type")
+	@Enumerated(EnumType.STRING)
+	private CommissionType commissioningType;
+	
+	@ElementCollection
+	@CollectionTable(name = "commission_values", joinColumns = @JoinColumn(name = "seller_id"))
+	@MapKeyColumn(name = "commission_key")
+	@Column(name = "commission_value", nullable = true, precision = 3, scale = 2)
+	private Map<BigDecimal, BigDecimal> commissionValues;
 	
 	@Basic
-	@Column(name = "sales_target", nullable = true)
-	private BigDecimal salesTarget;
+	@Column(name = "commission", precision = 3, scale = 2, nullable = true)
+	private BigDecimal commission;
+	
+	@Basic
+	@Column(name = "discount_limit")
+	private BigDecimal discountLimit;
+	
+	@Basic
+	@Column(name = "number_of_installments_contract")
+	private Integer numberOfInstallmentsContract;
+	
+	@Basic
+	@OneToOne
+	@JoinColumn(name = "users_id", unique = true)
+	private Users users;
 }
