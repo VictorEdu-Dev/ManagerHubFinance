a1f529158f57405738f0d106a1ec9462643605de
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:36:01 -0300

    remove field roles

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Function.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Function.java
@@ -1,11 +1,8 @@
 package org.vedu.managerhubfinance.persistence.model.groupcollaborator;
 
-import java.util.List;
-
 import org.vedu.managerhubfinance.persistence.model.PropertiesEntity;
 
 import jakarta.persistence.Basic;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,20 +10,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(exclude = { "roles"})
 @Builder
 @Table(name = "function")
 @Entity
@@ -39,13 +33,7 @@ public class Function extends PropertiesEntity {
 	@Column(name = "name", nullable = false, length = 50)
 	private String name;
 	
-	@Lob
-	@Column(name = "description", nullable = true, length = 100)
+	@Basic(fetch = FetchType.LAZY) @Lob
+	@Column(name = "description", nullable = false, length = 200)
 	private String description;
-	
-	@ManyToMany(
-			mappedBy = "functions", 
-			fetch = FetchType.LAZY, 
-			cascade = CascadeType.ALL)
-	private List<Roles> roles;
 }
