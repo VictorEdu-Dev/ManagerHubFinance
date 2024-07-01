bdc948a40b489429821637cc4a61fad8508458c0
Victor Eduardo <victoreduardodev@gmail.com>
2024-07-01 12:53:27 -0300

    remove unnecessary relationship

--- src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Roles.java
+++ src/main/java/org/vedu/managerhubfinance/persistence/model/groupcollaborator/Roles.java
@@ -1,34 +1,26 @@
 package org.vedu.managerhubfinance.persistence.model.groupcollaborator;
 
-import java.util.List;
-
 import org.vedu.managerhubfinance.persistence.model.PropertiesEntity;
 
 import jakarta.persistence.Basic;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(exclude = {"functions"})
 @Builder
 @Table(name = "roles")
 @Entity
@@ -41,16 +33,7 @@ public class Roles extends PropertiesEntity {
 	@Column(name = "name", nullable = false, length = 50)
 	private String name;
 	
-	@Lob
-	@Column(name = "description", nullable = true, length = 100)
+	@Basic(fetch = FetchType.LAZY) @Lob
+	@Column(name = "description", nullable = false, length = 200)
 	private String description;
-	
-	@ManyToMany(
-			fetch = FetchType.LAZY,
-			cascade = CascadeType.ALL)
-	@JoinTable(
-			name = "roles_function", 
-			joinColumns = @JoinColumn(name = "roles_id"),
-			inverseJoinColumns = @JoinColumn(name = "function_id"))
-	private List<Function> functions;
 }
